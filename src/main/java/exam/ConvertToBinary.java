3b0f1a48ca80b394b9ac922c24992e0098e720b6
Shubham Dharak <44456805+shubhamdharak@users.noreply.github.com>
2021-06-22 13:12:40 +0530

    code updated

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,18 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    if(n<0)
+      throw new ArithmeticException("Invalid Input");
+    int digit =n;
+    String str = "";
+    for(int i=0; digit>0; i++) {
+      str += digit%2;
+      digit = digit/2;
+    }
+    String binary = "";
+    for(int i=str.length()-1; i>=0; i--) {
+      binary += str.charAt(i);
+      return binary;
+    }
   }
 }
